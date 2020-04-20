5ddffb6a300d436bd38f354d3aa4863b9c43f29f
Vadim Prudnichenko <vadim.prudnichenko@gmail.com>
2020-04-20 10:55:13 +0000

    Add script push.sh

---
+++ push.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# ---------------------------------------
+# Description: push to remote repository
+# Written by : zuwawrskej
+# ---------------------------------------
+
+# Add all added/modified files
+echo "Add all added/modified files"
+git add .
+
+# Check added files to commit
+echo "Current status is:"
+git status
+
+# Make commit
+echo 'Enter the commit message:'
+read commit
+git commit -m "$commit"
+
+# Check remote repository
+echo "Remote repository is:"
+git remote -v
+
+# Push to remote
+read -n1 -p -r "Do you want to push to master?" push
+
+case $push in
+y|Y git push origin master ;;
+n|N echo "Exiting" exit ;;
+esac
