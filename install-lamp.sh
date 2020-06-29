fbfbaad9f83998ead68515bb6197fc103664d28c
Vadim Prudnichenko <v.prudnichenko@itransition.com>
2020-06-29 17:19:13 +0000

    add new script install-lamp.sh

---
+++ install-lamp.sh
@@ -0,0 +1,58 @@
+
+#!/bin/bash
+# -----------------------------------------------------#
+# Description: install and configure LAMP Debian/Ubuntu
+# Written by : v.prudnichenko
+# -----------------------------------------------------#
+
+FILE=/var/www/html/test-php.php
+
+# Check root Rights
+if [ "$UID" != "0" ]; then
+    [ ! -f /usr/bin/sudo ] && echo "Install sudo!" && exit 1;
+    if sudo echo &> /dev/null; then
+        echo "Self rerun with sudo..."
+        exec sudo bash $0
+    else
+        echo "Script must be runned as root or by user with sudo permissions!" && exit 1;
+    fi
+fi
+
+# Update packages
+apt update && apt upgrade -yqq
+
+# Install LAMP
+if apt install -y --reinstall git curl wget mc apache2 mariadb mariadb-server; then
+        apt install -y --reinstall php libapache2-mod-php php-cli php-mysqlnd php-json php-gd php-ldap \
+        php-odbc php-pdo php-opcache php-pear php-xml php-xmlrpc php-mbstring php-snmp \
+        php-soap php-zip
+fi
+
+# Pre-setup mariadb-server
+/usr/bin/mysql_secure_installation
+
+# Running/restart mariadb service
+systemctl status mysql | grep 'active (running)' > /dev/null 2>&1
+
+if [ $? != 0 ]
+        then
+                systemctl restart mysql > /dev/null
+fi
+
+# Running/restart apache2 service
+systemctl status apache2  | grep 'active (running)' > /dev/null 2>&1
+
+if [ $? != 0 ]
+        then
+                systemctl restart apache2 > /dev/null
+fi
+
+# Create file test-php.php and test php
+echo "<?php phpinfo(); ?>" > /var/www/html/test-php.php
+
+# Check file test-php.php
+if [ -f "$FILE" ]; then
+   echo "$FILE exist"
+else
+   echo "$FILE doesn't exist"
+fi
